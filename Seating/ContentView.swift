250428cdd3eeb5ab9c7ab1212b91a0c052cf9bed
Maya-svg <mayaf388@gmail.com>
2020-02-16 15:51:21 -0800

    Initial Commit

---
+++ Seating/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Seating
+//
+//  Created by Maya Fenelon on 2/16/20.
+//  Copyright © 2020 Maya Fenelon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
